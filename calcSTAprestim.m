87c508f0433134e761426fb6c3ddbcd613cb371b
YY <ys7yoo@gmail.com>
2018-05-28 22:25:12 +0900

    cleaned up Matlab code

--- calcSTAprestim.m
+++ calcSTAprestim.m
@@ -1,23 +1,24 @@
-function [STA, gridT] = calcSTAprestim(stim, stimTime, spikeTime, W, fps) 
+function [STA, gridT] = calcSTAprestim(stim, stimTime, spikeTime, W, fps)
 
-% stimTime - stimlus time 
-% spikeTime - spike Time 
+% stimTime - stimlus time
+% spikeTime - spike Time
 % W -     % window size in sec
 
 
 gridT = -W:1/fps:0;
+numTap = length(gridT);
 
 
 
 
 % % % convert to index
 % % spikeTimeIdx = round(spikeTime*fps);
-% % 
+% %
 % % [height, width, T] = size(stim);
-% % 
+% %
 % % if (max(spikeTimeIdx) > T)
 % %     warning('Maximum spike time exceeds recoding time. Check!!!')
-% %     
+% %
 % %     % [QUICK FIX]
 % %     % remove spikeTimeIdx out of stim range
 % %     spikeTimeIdx = spikeTimeIdx(spikeTimeIdx<=T);
@@ -32,29 +33,25 @@ stimTriggeredSpike = [];
 
 %numSpike = length(spikeTimeIdx);
 
-%stimTriggeredSpike = zeros (height,width,W,numSpike);
+%stimTriggeredSpike = zeros(height,width,W,numSpike);
 cnt = 1;
 for st=1:length(spikeTime)
-  if spikeTime(st) > stimTime(W*fps+1) && spikeTime(st) < stimTime(end)
-    PreStims = [];
-    PreStimInx = [];
-    Stims = spikeTime(st)-stimTime;
-    for i=1:length(Stims)
-      if Stims(i)>0
-        PreStims(1,i) = Stims(i);
-      end
+    if spikeTime(st) > stimTime(W*fps+1) && spikeTime(st) < stimTime(end)
+        
+        % find indices of pre-stim
+        idxPreStim=find(stimTime<spikeTime(st));
+        
+        % find most recent numTap indices
+        idxPreStim = idxPreStim(end-numTap+1:end);
+        
+        % grab stim & store to stimTriggeredSpike
+        stimTriggeredSpike(:,:,cnt) = stim(idxPreStim,:);
+        
+        cnt = cnt + 1;
     end
-        minPreStim = min(PreStims);
-        PreStimInx = find(Stims==minPreStim);
-       for k=1:length(gridT)
-        stimTriggeredSpike(k,:,cnt) = stim(PreStimInx-length(gridT)+k,:); 
-       end
-        cnt = cnt + 1;   
-  end
 end
 
-STA = mean(stimTriggeredSpike,3); % 2-dim array (height x width) x W
+STA = mean(stimTriggeredSpike,3); % 2-dim array (height x width) x numTap
 
 
 
-                                 
