cbba1105d7b6dc9c41dd9ddd8cd16e679e7e4a10
yyoo@MP4 <ys7yoo@gmail.com>
2019-01-28 03:28:39 +0900

    plot STA and mask

--- tools/plot_mask.m
+++ tools/plot_mask.m
@@ -1,8 +1,21 @@
-function plot_mask(STA, height, width)
+function plot_mask(STA, sig_smooth, height, width)
 
-if nargin<2
+if nargin<3
+    num_pixels = size(STA,2);
     height = sqrt(num_pixels);
     width = sqrt(num_pixels);
+end
+
+
+
+
+
+
+%% smooth STA for each slice 
+if sig_smooth > 0
+    STA_smoothed = smooth_STA_slice(STA, sig_smooth, height, width);
+else
+    STA_smoothed = STA;
 end
 
 
@@ -10,41 +23,57 @@ end
 T = size(STA,1);
 sig = std(STA(:));
 
-
-
-%% smooth STA for each slice 
-STA_smoothed = smooth_STA_slice(STA, height, width);
-
-
-%% 
 r=floor(sqrt(T));
 c=ceil(T/r);
+
 
 figure(1)
 clf; cnt = 1;
 for t=1:T
     subplot(r,c,cnt)
-    imshow(reshape(STA_smoothed(t,:,:),height,width)>0.5+2.58*sig)
+    
+    imagesc(reshape(STA_smoothed(t,:,:),height,width), [0.2 0.8])
+    colormap gray
+    
     cnt = cnt + 1;
 end
 
 set(gcf, 'paperposition', [0 0 24 18])
 set(gcf, 'papersize', [24 18])
 
-saveas(gcf, sprintf('mask_pos.png'))
-saveas(gcf, sprintf('mask_pos.pdf'))
+saveas(gcf, sprintf('sta_slice.png'))
+saveas(gcf, sprintf('sta_slice.pdf'))
+
 
 
 figure(2)
 clf; cnt = 1;
 for t=1:T
     subplot(r,c,cnt)
-    imshow(reshape(STA_smoothed(t,:,:),height,width)<0.5-2.58*sig)
+    
+    mask_on = reshape(STA_smoothed(t,:,:),height,width)>0.5+2.58*sig;
+    mask_off = reshape(STA_smoothed(t,:,:),height,width)<0.5-2.58*sig;
+    imshow(mask_on | mask_off)
     cnt = cnt + 1;
 end
 
 set(gcf, 'paperposition', [0 0 24 18])
 set(gcf, 'papersize', [24 18])
 
-saveas(gcf, sprintf('mask_neg.png'))
-saveas(gcf, sprintf('mask_neg.pdf'))
+saveas(gcf, sprintf('mask.png'))
+saveas(gcf, sprintf('mask.pdf'))
+
+% 
+% figure(2)
+% clf; cnt = 1;
+% for t=1:T
+%     subplot(r,c,cnt)
+%     imshow(mask_off)
+%     cnt = cnt + 1;
+% end
+% 
+% set(gcf, 'paperposition', [0 0 24 18])
+% set(gcf, 'papersize', [24 18])
+% 
+% saveas(gcf, sprintf('mask_neg.png'))
+% saveas(gcf, sprintf('mask_neg.pdf'))
