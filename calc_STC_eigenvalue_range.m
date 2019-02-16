77d34ad765e905647502f0d07f4fd3cc5b3eaf06
yyoo@MP4 <ys7yoo@gmail.com>
2019-02-17 03:54:34 +0900

    calc confidence interval for each eigen value

--- calc_STC_eigenvalue_range.m
+++ calc_STC_eigenvalue_range.m
@@ -1,4 +1,4 @@
-function  [ev_range, evs, stc, sta] = calc_STC_eigenvalue_range(stim, spikeTrain, num_samples_per_window, num_random_shift, random_shift_range)
+function  [ev_upper, ev_lower, evs, num_spikes] = calc_STC_eigenvalue_range(stim, spikeTrain, num_samples_per_window, num_random_shift, random_shift_range, sta_to_project_out)
 
     %% set default params for shuffle
     if nargin<4
@@ -9,89 +9,63 @@ function  [ev_range, evs, stc, sta] = calc_STC_eigenvalue_range(stim, spikeTrain
         random_shift_range = num_samples_per_window * [1 10];
     end
     
+    if nargin<6
+        sta_to_project_out = [];
+    end
     
-    ev_min=[];
-    ev_max=[];
-    
-    ev_mean=[];
-    ev_var=[];
-    
-    
+    % to store eigen values for each repeat
+    dim = size(stim,2) * num_samples_per_window;
+    evs = zeros(dim, num_random_shift);
+            
     for n=1:num_random_shift
-        disp('.')
+        %disp('.')
         %% random shift
         random_shift = round(diff(random_shift_range) * rand(1)  + random_shift_range(1));
     
         
         %% calc STA and STC
-        [sta, stc] = calc_STA_and_STC(stim(random_shift:end,:), spikeTrain(1:end-random_shift+1,:), num_samples_per_window);
-
-
-
-        %% analyze STC result
-        if iscell(stc)
-            for i=1:size(stc,1)
-                [~, D, ~] = svd(stc{i});
-                ev{i} = diag(D);
+        [~, ev] = calc_STA_and_STC(stim(random_shift:end,:), spikeTrain(1:end-random_shift+1,:), num_samples_per_window, sta_to_project_out);
                 
-                % store max and min 
-                ev_max = [ev_max max(ev{i})];
-                ev_min = [ev_min min(ev{i})];
-                
-                ev_mean = [ev_mean mean(ev{i})];
-                ev_var = [ev_var var(ev{i})];
-                
-                
-            end
-        else
-            ev = eig(stc);
-            
-            ev = ev(ev>1e-5);
-%             [~, D, ~] = svd(stc);
-%             ev = diag(D);
-
-            
-            % store max and min 
-            ev_max = [ev_max max(ev)];
-            ev_min = [ev_min min(ev)];
-            
-            ev_mean = [ev_mean mean(ev)];
-            ev_var = [ev_var var(ev)];               
-        end
-        
         % store evs
-        if nargout>1
-            evs{n} = ev;    
-            % hist(reshape(cell2mat(evs{n}),[],1))
+        evs(1:length(ev),n) = ev;    
+
+        % store evs
+        if nargout>3
+            num_spikes(:,n) = sum(spikeTrain(1:end-random_shift+1,:));
         end
                         
     end
-    
-    %% for debug
-    if 1==0
-        %%
-        close all
-        subplot(121)
-        hist(ev_min)
-        subplot(122)
-        hist(ev_max)
-    end
 
-    
-%     % Option 1. calc range by min and max 
-%     ev_range=[mean(ev_min) mean(ev_max)];
-    
-    % Option 2. calc range by mean and var
-    
-    ev_mean_global = mean(ev_mean);
-    ev_var_global = mean(ev_var);
-    ev_std = sqrt(ev_var_global);
 
-%     % 95% significance interval
-%     ev_range = ev_mean_global + ev_std*1.96*[-1 1];
+    % calc confidence interval using non-zero eigen values
+    evs(evs<1e-6) = nan;
+
+
+    mm = nanmean(evs,2);
+    ss = nanstd(evs,[],2);
     
-    % 95% significance interval
-    ev_range = ev_mean_global + ev_std*2.54*[-1 1];
+    % 99% confidence interval for each eig
+    ev_upper = mm+2.576*ss;
+    ev_lower = mm-2.576*ss;
+    
+
+%     % 99% significance interval
+%     ev_range = [mm(end)-2.576*ss(end) mm(1)+2.576*ss(1)]; 
+    
+% %     % Option 1. calc range by min and max 
+% %     ev_range=[mean(ev_min) mean(ev_max)];
+%     
+%     % Option 2. calc range by mean and var
+%     
+%     ev_mean_global = mean(ev_mean);
+%     ev_var_global = mean(ev_var);
+%     ev_std = sqrt(ev_var_global);
+% 
+% %     % 95% significance interval
+% %     ev_range = ev_mean_global + ev_std*1.96*[-1 1];
+%     
+%     % 99% significance interval
+%     ev_range = ev_mean_global + ev_std*2.58*[-1 1];
     
     
     
@@ -104,6 +78,50 @@ return
 ev_range = calc_STC_eigenvalue_range(stim, spikeTrain(:,n), STA_num_samples, 50, [1 10]);
 
 
+%% Further analysis (with mask)
+[ev_range, evs] = calc_STC_eigenvalue_range(stim(:,mask(:)), spike_train(:,n), sta_num_samples, 50, sta_num_samples*[10 50]);
+
+
+%%
+mean(evs(:))
+
+clf
+hist(evs(:),100)
+box off
+
+evs_mean = mean(evs(:))
+hold on
+YLIM = get(gca,'ylim');
+plot(evs_mean*[1 1], YLIM, 'r--');
+
+
+%%
+subplot(221)
+%hist(mean(evs,2))
+hist(evs(:),100)
+
+hold on
+YLIM = get(gca,'ylim');
+plot(evs_mean*[1 1], YLIM, 'r--');
+plot(ev_range(1)*[1 1], YLIM, 'r--');
+plot(ev_range(2)*[1 1], YLIM, 'r--');
+plot(mean(min(evs))*[1 1], YLIM, 'g--');
+plot(mean(max(evs))*[1 1], YLIM, 'g--');
+
+
+subplot(222)
+hist(std(evs,[],2))
+title('histogram of std')
+
+subplot(223)
+hist(min(evs))
+mean(min(evs))
+title('histogram of min')
+
+subplot(224)
+hist(max(evs))
+mean(max(evs))
+title('histogram of max')
 %% Further analysis
 [ev_range, evs, stc, sta] = calc_STC_eigenvalue_range(stim, spikeTrain(:,n), STA_num_samples, 50, [1 10]);
 
