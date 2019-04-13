c18d7c96a18e9518f1c2d52e6c6e42f55136ed11
yyoo@MP4 <ys7yoo@gmail.com>
2019-04-13 11:03:34 +0900

    separated loading routine

---
+++ tools/load_data.m
@@ -0,0 +1,107 @@
+function [stim, spike_train, channel_names] = load_data_coupling(folder_name_by_date)
+
+%% 
+base_folder_name = fullfile('coupling_data', folder_name_by_date);
+
+%% parse info from StimInfo file
+find_stim_info_filename = dir(fullfile(base_folder_name,'Stiminfo*'));
+stim_info_filename = find_stim_info_filename.name;
+
+
+[num_pixels, distance_between_electrodes, sampling_rate] = parse_stim_info_filename(stim_info_filename)
+
+
+width = num_pixels;
+height = num_pixels;
+
+%% load 'StimInfo'
+load(fullfile(base_folder_name,stim_info_filename));
+
+T = length(StimInfo);
+
+stim = cell2mat(StimInfo);
+
+%size(stim) % 9000 x num_pixels x num_pixels
+
+
+%%  plot some sample stimulus
+clf
+subplot(131)
+imshow(reshape(stim(1,:),[height, width]))
+xlabel('x');ylabel('y');axis xy
+
+subplot(132)
+imshow(reshape(stim(2,:),[height, width]))
+xlabel('x');ylabel('y');axis xy
+
+subplot(133)
+imshow(reshape(stim(3,:),[height, width]))
+xlabel('x');ylabel('y');axis xy
+
+%%  load SpikeTrain Dataset & save txt
+spike_train_folder_name = fullfile(base_folder_name, sprintf('SpikeTrain_%s/SpikeTrain_ND2_%dpix_%dum_%dHz',folder_name_by_date,num_pixels, distance_between_electrodes, sampling_rate));
+spike_train_folder_name = [spike_train_folder_name '/']
+
+matFiles = dir([spike_train_folder_name '*.mat']);
+if isempty(matFiles)
+    error(sprintf('Cannot load spike train data from %s',spike_train_folder_name))
+end
+matFiles = {matFiles.name};
+
+cnt = 1;
+for i = 1:length(matFiles)
+    
+    fileName = matFiles{i};    
+    channel_name = fileName(1:end-4);  % remove '.mat'
+    if ~strcmp(channel_name, 'A1a')
+        channel_names{cnt} = channel_name;    % save channel names (except A1a)
+        cnt = cnt + 1;  % number of found channels
+    end
+    
+    fprintf('Loading %s\n', fileName)
+    load(fullfile(spike_train_folder_name, fileName))
+    
+    % save to txt file
+    save([spike_train_folder_name channel_name '.txt'], channel_name, '-ascii')
+    
+end
+
+
+%% For further analysis, need to convert spike time to spike train
+
+N = length(channel_names); % number of neurons 
+
+binStim = size(stim,1);
+assert (binStim==length(A1a))
+%spike_train = zeros(binStim,N);
+spike_train = sparse(binStim,N);
+
+for n = 1:N
+    
+    channel_name = channel_names{n};
+    disp(channel_name)
+    spike_time=eval(channel_name);
+    
+    
+    % let's check for each stim time bin
+    for i = 2:binStim
+        t0 = A1a(i-1);
+        t1 = A1a(i);
+
+        % find stim time that occured during t0 and t1
+        idx = find(spike_time>t0 & spike_time<=t1);
+
+        spike_train(i,n) = length(idx);  % multiple spikes may occur in a bin
+%         if ~isempty(idx)
+%             %disp('found')
+%             spike_train(i,n) = 1;
+%         end
+
+    end
+end
+
+
+clf
+imagesc(spike_train', [0 1]); colormap gray
+
+
