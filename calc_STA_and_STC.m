f370a18e2fe4b74e47cb0046a11e08787ce549a2
yyoo@MB <>
2019-09-16 12:40:04 +0900

    detail analysis for each channel

--- calc_STA_and_STC.m
+++ calc_STA_and_STC.m
@@ -13,6 +13,9 @@ end
 %% store spike-tiggered stims in to X with spike numbers in spikes
 [X, spikes, num_total_spikes] = collect_spike_triggered_stim(stim, spike_train, n);
 
+% save here for further analysis
+%save ch33b.mat X spikes num_total_spikes
+
 %% calc STA
 sta = spikes'*X/num_total_spikes;
 
@@ -52,8 +55,60 @@ end
 return 
 
 
+%% for detail analysis for each channel
+% data saved by the following command: save ch33b.mat X spikes num_total_spikes
+
+clear
+load ch33b.mat
+
+% calc STA
+sta = spikes'*X/num_total_spikes;
+
+% calc STFC
+[stc_eig_val, stc_eig_vec, S] = calc_STC(X, spikes);
+                        
+stc_eig_vec = flip_column_sign(stc_eig_vec, sta);  % flip according to sta (for better visualization)
+
+% plot eig val
+clf
+subplot(221)
+plot(stc_eig_val, 'o--')
+box off
+
+subplot(222)
+hist(stc_eig_val)
+box off
 
 
+% calc scores for first r dimension
+r = 2
+score = X*stc_eig_vec(:,1:r)
+
+subplot(223)
+sc1 = scatter(score(:,1), score(:,2), '.'); %, '.', 'alpha', 0.2)
+sc1.MarkerEdgeAlpha=0.4;
+
+hold on
+cov12 = cov(score(:,1), score(:,2))
+plot_ellipse([0 0], cov12)
+
+xlabel('ev1')
+ylabel('ev2')
+axis equal
+
+axis ([-2.5 2.5 -2 2])
+
+subplot(224)
+sc1 = scatter(score(:,end), score(:,end-1), '.'); %, '.', 'alpha', 0.2)
+sc1.MarkerEdgeAlpha=0.4;
+
+hold on
+plot_ellipse([0 0], cov(score(:,end), score(:,end-1)))
+
+xlabel(sprintf('ev%d',length(stc_eig_val)))
+ylabel(sprintf('ev%d',length(stc_eig_val)-1))
+axis equal
+axis ([-2.5 2.5 -2 2])
 
 
 %% to debug
